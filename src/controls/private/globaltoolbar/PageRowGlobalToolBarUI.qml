trunghieu2570/kirigami

commit 52ce01c7e5a9e8ded58bb4241004062f638266d6

Fix navigation buttons on layers

src/controls/private/globaltoolbar/PageRowGlobalToolBarUI.qml
@@ -78,7 +78,7 @@ Kirigami.AbstractApplicationHeader {
             Layout.leftMargin: leftHandleAnchor.visible ? Kirigami.Units.smallSpacing : 0
 
             // TODO KF6: make showNavigationButtons an int, and replace with strict === equality
-            visible: (globalToolBar.showNavigationButtons != Kirigami.ApplicationHeaderStyle.NoNavigationButtons || root.layers.depth > 1)
+            visible: (globalToolBar.showNavigationButtons != Kirigami.ApplicationHeaderStyle.NoNavigationButtons || applicationWindow().layers.depth > 1)
                 && globalToolBar.actualStyle !== Kirigami.ApplicationHeaderStyle.None
 
             Layout.maximumWidth: visibleChildren.length > 0 ? Layout.preferredWidth : 0

commit d4f4a67cc6b1cd023c657e5745dedbe93d1b8ef3

Fix type error introduced in 52ce01c7e5a9e8ded58bb4241004062f638266d6

src/controls/private/globaltoolbar/PageRowGlobalToolBarUI.qml
@@ -78,7 +78,7 @@ Kirigami.AbstractApplicationHeader {
             Layout.leftMargin: leftHandleAnchor.visible ? Kirigami.Units.smallSpacing : 0
 
             // TODO KF6: make showNavigationButtons an int, and replace with strict === equality
-            visible: (globalToolBar.showNavigationButtons != Kirigami.ApplicationHeaderStyle.NoNavigationButtons || applicationWindow().layers.depth > 1)
+            visible: (globalToolBar.showNavigationButtons != Kirigami.ApplicationHeaderStyle.NoNavigationButtons || applicationWindow().pageStack.layers.depth > 1)
                 && globalToolBar.actualStyle !== Kirigami.ApplicationHeaderStyle.None
 
             Layout.maximumWidth: visibleChildren.length > 0 ? Layout.preferredWidth : 0