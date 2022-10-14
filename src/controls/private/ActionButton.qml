trunghieu2570/kirigami

commit 3fd3f54766784bcbc5099759f488ddbc0c7d120f

Replace sizeForLabels with Units.iconSizes.medium/large where relevant
Some components have comments indicating that using sizeForLabels was a temporary
workaround and should be ported to use standard icon sizes after a specific Plasma
version. That version was released a while ago, so it's time to make the change.

src/controls/private/ActionButton.qml
@@ -94,10 +94,8 @@ Item {
         id: button
         x: root.width/2 - button.width/2
 
-        // We have a mismatch in releases in removing the mobile x1.5 sizing (kirigami is part of frameworks, but styles are part of plasma releases)
-        // Remove after Plasma 5.23 is released, and switch back to Units.iconSizes.medium/large
-        property int mediumIconSizing: Units.iconSizes.sizeForLabels * 2
-        property int largeIconSizing: Units.iconSizes.sizeForLabels * 3
+        property int mediumIconSizing: Units.iconSizes.medium * 2
+        property int largeIconSizing: Units.iconSizes.large * 3
 
         anchors.bottom: edgeMouseArea.bottom
 

src/controls/private/GlobalDrawerActionItem.qml
@@ -28,9 +28,7 @@ AbstractListItem {
             color: modelData.icon.color
             source: modelData.icon.name || modelData.icon.source
 
-            // We have a mismatch in releases in removing the mobile x1.5 sizing (kirigami is part of frameworks, but styles are part of plasma releases)
-            // Remove after Plasma 5.23 is released, and switch back to Units.iconSizes.medium/large
-            property int mediumIconSizing: Units.iconSizes.sizeForLabels * 2
+            property int mediumIconSizing: Units.iconSizes.medium * 2
 
             property int size: Settings.isMobile ? mediumIconSizing : Units.iconSizes.smallMedium
             Layout.minimumHeight: size

commit ca3091d33e4cfa4430a0666e7033dfff78e870cb

Fix ginormous FABs
Fixes a regression introduced by https://invent.kde.org/frameworks/kirigami/-/commit/3fd3f54766784bcbc5099759f488ddbc0c7d120f where floating action buttons become ginormous (Android 12 sized)

src/controls/private/ActionButton.qml
@@ -94,8 +94,8 @@ Item {
         id: button
         x: root.width/2 - button.width/2
 
-        property int mediumIconSizing: Units.iconSizes.medium * 2
-        property int largeIconSizing: Units.iconSizes.large * 3
+        property int mediumIconSizing: Units.iconSizes.medium
+        property int largeIconSizing: Units.iconSizes.large
 
         anchors.bottom: edgeMouseArea.bottom
 

src/controls/private/GlobalDrawerActionItem.qml
@@ -28,7 +28,7 @@ AbstractListItem {
             color: modelData.icon.color
             source: modelData.icon.name || modelData.icon.source
 
-            property int mediumIconSizing: Units.iconSizes.medium * 2
+            property int mediumIconSizing: Units.iconSizes.medium
 
             property int size: Settings.isMobile ? mediumIconSizing : Units.iconSizes.smallMedium
             Layout.minimumHeight: size