540c9bcec9e81c0bca71fac7ff71051f1d51b7dc
Devin Lin <espidev@gmail.com>
2022-10-25 11:32:19 -0400

    FloatingActionButton: Add component

--- src/kirigamiplugin.cpp
+++ src/kirigamiplugin.cpp
@@ -328,6 +328,9 @@ void KirigamiPlugin::registerTypes(const char *uri)
     // 2.20
     qmlRegisterType(componentUrl(QStringLiteral("SelectableLabel.qml")), uri, 2, 20, "SelectableLabel");
 
+    // 2.21
+    qmlRegisterType(componentUrl(QStringLiteral("FloatingActionButton.qml")), uri, 2, 21, "FloatingActionButton");
+
     qmlProtectModule(uri, 2);
 }
 
