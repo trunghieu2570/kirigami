7679358532f16cafb3379c77c59b488c064b927c
Carl Schwan <carl@carlschwan.eu>
2023-01-07 13:48:16 +0100

    Improve default template
    - Remove About wrapper
    - Add code for windows and android compatibility
    - Cleanup some minor issues in the QML
    
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>
    (cherry picked from commit db512810e2e076bdef9bda60079cc66a63c3de2f)

--- templates/kirigami/src/main.cpp
+++ templates/kirigami/src/main.cpp
@@ -3,9 +3,14 @@
     SPDX-FileCopyrightText: %{CURRENT_YEAR} %{AUTHOR} <%{EMAIL}>
 */
 
+#ifdef Q_OS_ANDROID
+#include <GuiQApplication>
+#else
 #include <QApplication>
+#endif
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QQuickStyle>
 #include <QUrl>
 
 #include "about.h"
@@ -17,46 +22,76 @@
 
 #include "%{APPNAMELC}config.h"
 
-Q_DECL_EXPORT int main(int argc, char *argv[])
+#ifdef Q_OS_ANDROID
+Q_DECL_EXPORT
+#endif
+int main(int argc, char *argv[])
 {
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+#ifdef Q_OS_ANDROID
+    QGuiApplication app(argc, argv);
+    QQuickStyle::setStyle(QStringLiteral("org.kde.breeze"));
+#else
     QApplication app(argc, argv);
+
+    // Default to org.kde.desktop style unless the user forces another style
+    if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
+        QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
+    }
+#endif
+
+#ifdef Q_OS_WINDOWS
+    if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+        freopen("CONOUT$", "w", stdout);
+        freopen("CONOUT$", "w", stderr);
+    }
+
+    QApplication::setStyle(QStringLiteral("breeze"));
+    auto font = app.font();
+    font.setPointSize(10);
+    app.setFont(font);
+#endif
+
+    KLocalizedString::setApplicationDomain("%{APPNAMELC}");
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
-    QCoreApplication::setApplicationName(QStringLiteral("%{APPNAME}"));
 
     KAboutData aboutData(
-                         // The program name used internally.
-                         QStringLiteral("%{APPNAME}"),
-                         // A displayable program name string.
-                         i18nc("@title", "%{APPNAME}"),
-                         // The program version string.
-                         QStringLiteral(%{APPNAMEUC}_VERSION_STRING),
-                         // Short description of what the app does.
-                         i18n("Application Description"),
-                         // The license this code is released under.
-                         KAboutLicense::GPL,
-                         // Copyright Statement.
-                         i18n("(c) %{CURRENT_YEAR}"));
+        // The program name used internally.
+        QStringLiteral("%{APPNAMELC}"),
+        // A displayable program name string.
+        i18nc("@title", "%{APPNAME}"),
+        // The program version string.
+        QStringLiteral(% {APPNAMEUC} _VERSION_STRING),
+        // Short description of what the app does.
+        i18n("Application Description"),
+        // The license this code is released under.
+        KAboutLicense::GPL,
+        // Copyright Statement.
+        i18n("(c) %{CURRENT_YEAR}"));
     aboutData.addAuthor(i18nc("@info:credit", "%{AUTHOR}"),
-                        i18nc("@info:credit", "Author Role"),
+                        i18nc("@info:credit", "Maintainer"),
                         QStringLiteral("%{EMAIL}"),
                         QStringLiteral("https://yourwebsite.com"));
+    about.setTranslator(i18nc("NAME OF TRANSLATORS", "Your names"), i18nc("EMAIL OF TRANSLATORS", "Your emails"));
     KAboutData::setApplicationData(aboutData);
+    QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.%{APPNAMELC}")));
 
     QQmlApplicationEngine engine;
 
     auto config = %{APPNAME}Config::self();
 
-    qmlRegisterSingletonInstance("org.kde.%{APPNAME}", 1, 0, "Config", config);
+    qmlRegisterSingletonInstance("org.kde.%{APPNAMELC}", 1, 0, "Config", config);
 
-    AboutType about;
-    qmlRegisterSingletonInstance("org.kde.%{APPNAME}", 1, 0, "AboutType", &about);
+    qmlRegisterSingletonType("org.kde.%{APPNAMELC}", 1, 0, "About", [](QQmlEngine *engine, QJSEngine *) -> QJSValue {
+        return engine->toScriptValue(KAboutData::applicationData());
+    });
 
     App application;
-    qmlRegisterSingletonInstance("org.kde.%{APPNAME}", 1, 0, "App", &application);
+    qmlRegisterSingletonInstance("org.kde.%{APPNAMELC}", 1, 0, "App", &application);
 
     engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
-    engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     if (engine.rootObjects().isEmpty()) {
         return -1;
