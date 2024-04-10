efb5b04f93da91961df105894c6a302772fde7d8
ivan tkachenko <me@ratijas.tk>
2024-04-11 00:09:35 +0600

    Padding: Remove old overridden contentItem from the visual hierarchy
    This is similar to what QQC2.Control does, except we can't reliably
    reset visibility (which is also transient). So let's only unset the
    parent, since we set it for a new item already.
    
    Fixes old items floating around parented to Padding item overlapping
    with a new content item and not being properly sized anymore.

--- src/padding.cpp
+++ src/padding.cpp
@@ -128,8 +128,15 @@ void Padding::setContentItem(QQuickItem *item)
         return;
     }
 
-    // Not unsetting old contentItem's parent unlike Control
-    d->disconnect();
+    // Not hiding old contentItem unlike Control, because we can't reliably
+    // restore it or force `visibile:` binding re-evaluation.
+    if (d->m_contentItem) {
+        d->disconnect();
+        // Ideally, it should only unset the parent iff old item's parent is
+        // `this`. But QtQuick.Controls/Control doesn't do that, and we don't
+        // wanna even more inconsistencies with upstream.
+        d->m_contentItem->setParentItem(nullptr);
+    }
 
     d->m_contentItem = item;
 
