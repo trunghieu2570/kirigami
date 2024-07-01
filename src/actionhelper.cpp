c4e24782e764532e31dc1e7635e3d697851ea2cd
Carl Schwan <carl@carlschwan.eu>
2024-07-01 11:44:43 +0200

    Fix static build
    actionhelper and copyhelper need to be statically linked to the main
    Kirigami module

--- src/actionhelper.cpp
+++ src/actionhelper.cpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Carl Schwan <carl@carlschwan.eu>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-#include "actionhelper_p.h"
+#include "actionhelper.h"
 
 ActionHelper::ActionHelper(QObject *parent)
     : QObject(parent)
