ad72afb2fa9851451ada27a4c0f13e05590e5278
Nicolas Fella <nicolas.fella@gmx.de>
2024-07-21 21:01:40 +0200

    Relicense ImageColors to LGPL
    Frameworks code must be LGPL

--- src/imagecolors.cpp
+++ src/imagecolors.cpp
@@ -2,7 +2,7 @@
  *  SPDX-FileCopyrightText: 2020 Marco Martin <mart@kde.org>
  *  SPDX-FileCopyrightText: 2024 ivan tkachenko <me@ratijas.tk>
  *
- *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
  */
 
 #include "imagecolors.h"
