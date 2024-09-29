d1072b0baf44be2d0e66d0b3138011a28e0dfa8c
Volker Krause <vkrause@kde.org>
2024-09-29 13:25:39 +0200

    Fix unit tests when using a static build
    We can remove a bunch of boilerplate here as Qt 6 comes with better
    support for this.

--- autotests/qmltest.cpp
+++ autotests/qmltest.cpp
@@ -4,34 +4,6 @@
  *  SPDX-License-Identifier: LGPL-2.0-or-later
  */
 
-#include <QQmlEngine>
 #include <QtQuickTest>
 
-#ifdef STATIC_MODULE
-#include "kirigamiplugin.h"
-Q_IMPORT_PLUGIN(KirigamiPlugin)
-#endif
-
-class KirigamiSetup : public QObject
-{
-    Q_OBJECT
-
-public:
-    KirigamiSetup()
-    {
-    }
-
-public Q_SLOTS:
-    void qmlEngineAvailable(QQmlEngine *engine)
-    {
-#ifdef STATIC_MODULE
-        KirigamiPlugin::getInstance().registerTypes(engine);
-#else
-        Q_UNUSED(engine)
-#endif
-    }
-};
-
-QUICK_TEST_MAIN_WITH_SETUP(Kirigami, KirigamiSetup)
-
-#include "qmltest.moc"
+QUICK_TEST_MAIN(Kirigami)
